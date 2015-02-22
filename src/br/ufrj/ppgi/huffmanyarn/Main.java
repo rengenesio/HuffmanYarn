fb9ac8c26af7f9ac3a2a24fcd36a80139fa25f74
Renan Iglesias <renan.iglesias@ufrj.br>
2015-02-22 14:17:20 -0300

    Added make-all

--- src/br/ufrj/ppgi/huffmanyarn/Main.java
+++ src/br/ufrj/ppgi/huffmanyarn/Main.java
@@ -12,51 +12,76 @@ import br.ufrj.ppgi.huffmanyarn.yarn.Client;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		long t, t1, t2;
-		String in, out, cb;
-		Configuration conf = new Configuration();
-		FileSystem fs = FileSystem.get(conf);
-
-		in = new String(args[0]);
-		out = new String(in);
-		cb = new String(in);
-		out += ".yarndir/compressed/";
-		cb += ".yarndir/codification";
-
-		try {
-			fs.delete(new Path(args[0] + ".yarndir"), true);
-		} catch(Exception ex) { }
+		if(args.length < 2) { throw new IllegalArgumentException("Falta(m) parametro(s)"); }
+		
+		boolean encoder = false;
+		boolean decoder = false;
+		
+		String fileName = args[0];
+		switch(args[1]) {
+		case "encoder":
+			encoder = true;
+			break;
 			
-
-		t1 = System.nanoTime();
-		Client client = new Client(args);
-		
-		if (client.run()) {
-			System.out.println("Compressão completa!");
+		case "decoder":
+			decoder = true;
+			break;
+			
+		case "both":
+			encoder = true;
+			decoder = true;
+			break;
 		}
-		else {
-			System.out.println("Erro durante a compressão");
+		
+		if(encoder) {
+			long t, t1, t2;
+			String in, out, cb;
+			Configuration conf = new Configuration();
+			FileSystem fs = FileSystem.get(conf);
+	
+			in = new String(args[0]);
+			out = new String(in);
+			cb = new String(in);
+			out += ".yarndir/compressed/";
+			cb += ".yarndir/codification";
+	
+			try {
+				fs.delete(new Path(args[0] + ".yarndir"), true);
+			} catch(Exception ex) { }
+				
+	
+			t1 = System.nanoTime();
+			Client client = new Client(args);
+			
+			if (client.run()) {
+				System.out.println("Compressão completa!");
+			}
+			else {
+				System.out.println("Erro durante a compressão");
+			}
+			
+			t2 = System.nanoTime();
+			t = t2 - t1;
+			System.out.println(t/1000000000.0 + " s (encoder)");
 		}
-		
-		t2 = System.nanoTime();
-		t = t2 - t1;
-		System.out.println(t/1000000000.0 + " s (encoder)");
 
-		in = new String(args[0]);
-		out = new String(in);
-		cb = new String(in);
-		in += ".yarndir/compressed/";
-		out += ".yarndir/decompressed";
-		cb += ".yarndir/codification";
-		
-		System.out.println(in);
-		System.out.println(out);
-		System.out.println(cb);
-		
-		t1 = System.nanoTime();
-		new Decoder(in, out, cb);
-		t2 = System.nanoTime();
-		t = t2 - t1;
-		System.out.println(t/1000000000.0 + " s (decoder)");
+		if(decoder) {
+			in = new String(args[0]);
+			out = new String(in);
+			cb = new String(in);
+			in += ".yarndir/compressed/";
+			out += ".yarndir/decompressed";
+			cb += ".yarndir/codification";
+			
+			System.out.println(in);
+			System.out.println(out);
+			System.out.println(cb);
+			
+			t1 = System.nanoTime();
+			new Decoder(in, out, cb);
+			t2 = System.nanoTime();
+			t = t2 - t1;
+			System.out.println(t/1000000000.0 + " s (decoder)");
+		}
 	}
 }
